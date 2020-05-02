6bd78642f44aab784e76bacdf155c37736ca185f
Owen Conti <owen@ohseemedia.com>
2020-05-02 09:30:53 -0600

    fix: Fix phpcs errors

--- src/Schemas/PostgresSchema.php
+++ src/Schemas/PostgresSchema.php
@@ -3,7 +3,6 @@
 namespace OhSeeSoftware\LaravelSchemaList\Schemas;
 
 use Illuminate\Database\ConnectionInterface;
-use Illuminate\Support\Facades\DB;
 
 class PostgresSchema implements SchemaContract
 {
@@ -22,13 +21,31 @@ class PostgresSchema implements SchemaContract
     public function getColumns(ConnectionInterface $connection, string $table): array
     {
         $output = $connection->table('information_schema.columns', 'c')
-                             ->select(['c.column_name as Field', 'c.data_type as Type', 'c.is_nullable as Nullable', 'tc.constraint_type as Key', 'c.column_default as "Default Value"', 'c.ordinal_position'])
-                             ->join('information_schema.key_column_usage AS kcu', 'kcu.column_name', '=', 'c.column_name', 'full')
-                             ->leftJoin('information_schema.table_constraints AS tc', 'tc.constraint_name', '=', 'kcu.constraint_name')
-                             ->where('c.table_name', $table)
-                             ->orderBy('c.ordinal_position', 'asc')
-                             ->distinct()
-                             ->get();
+            ->select([
+                'c.column_name as Field',
+                'c.data_type as Type',
+                'c.is_nullable as Nullable',
+                'tc.constraint_type as Key',
+                'c.column_default as "Default Value"',
+                'c.ordinal_position'
+            ])
+            ->join(
+                'information_schema.key_column_usage AS kcu',
+                'kcu.column_name',
+                '=',
+                'c.column_name',
+                'full'
+            )
+            ->leftJoin(
+                'information_schema.table_constraints AS tc',
+                'tc.constraint_name',
+                '=',
+                'kcu.constraint_name'
+            )
+            ->where('c.table_name', $table)
+            ->orderBy('c.ordinal_position', 'asc')
+            ->distinct()
+            ->get();
 
         return collect($output)->map(function ($value) {
             unset($value->ordinal_position);
